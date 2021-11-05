2e758a54fea80e5fc9e3dd6d38b3b6574ec60a8c
Sergei Martushev <martuschev.sam@yandex.ru>
2021-11-05 21:07:29 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let s = amount - contribution;
+  let n = Math.ceil((date - Date.now())/(24 * 60 * 60 * 1000 * 30.42));
+  let p = (percent / 100) * (1 / 12);
 
-  // код для задачи №2 писать здесь
+  let monthlyPayment = s * (p + p / (((1 + p) ** n)-1));
+  totalAmount = (monthlyPayment * n).toFixed(2)
 
   return totalAmount;
 }
