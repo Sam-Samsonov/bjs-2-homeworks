05b561994ee6c7fa4dd5cf7dd8f0fa4c882fbe9d
Sergei Martushev <martuschev.sam@yandex.ru>
2021-11-13 13:51:42 +0300

    hw-2 first commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,62 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+  }
+
+  avg = +((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for (n = 0; n < arrOfArr.length; n++) {
+    if (func(arrOfArr[n]) > max) {
+      max = func(arrOfArr[n]);
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  let dif;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }else if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  dif = max - min;
+
+  return dif;
 }
