63e9ae5a315b681f6e2279a85abac819f7d85331
Sergei Martushev <martuschev.sam@yandex.ru>
2021-11-15 15:32:14 +0300

    hw-3 first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.every((item) => arr1[item] === arr2[item]);
 
-  return result; // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((item) => (item > 0) && (item % 3 === 0)).map((item) => item * 10);
 
-  return resultArr; // array
+  return resultArr;
 }
