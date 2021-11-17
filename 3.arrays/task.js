3d0c4bab64e11b61a321adbf57689c63b212a0bb
Sergei Martushev <martuschev.sam@yandex.ru>
2021-11-17 15:19:00 +0300

    third commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,7 @@
 function compareArrays(arr1, arr2) {
-
-  return result = arr1.every(elem => (arr2[elem] === arr1[elem]) && (arr2.length === arr1.length));
-
-//  return result = arr1.every((item) => arr1[item] === arr2[item]);
+  return arr1.length === arr2.length && arr1.every((elem, index) => elem === arr2[index]);
 }
 
 function advancedFilter(arr) {
-
-  return resultArr = arr.filter((elem) => (elem > 0) && (elem % 3 === 0)).map((elem) => elem * 10);
+  return arr.filter((elem) => (elem > 0) && (elem % 3 === 0)).map((elem) => elem * 10);
 }
