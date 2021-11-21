57e42b791cb2354df0bf7bf272fc620e31cdf821
Sergei Martushev <martuschev.sam@yandex.ru>
2021-11-21 10:17:48 +0300

    test

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,26 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name,
+  this.gender = gender,
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+   this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (arguments) {
+  if (this.marks === undefined) {
+    this.marks = Array.from(arguments);
+  } else {
+    this.marks.push(Array.from(arguments));
+  }
+}
+
