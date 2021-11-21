c4c90c5c718b964a5980351a97322402347d5013
Sergei Martushev <martuschev.sam@yandex.ru>
2021-11-21 14:33:35 +0300

    hw-4 first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -16,11 +16,20 @@ Student.prototype.addMark = function (mark) {
   }
 }
 
-Student.prototype.addMarks = function (arguments) {
+Student.prototype.addMarks = function () {
   if (this.marks === undefined) {
     this.marks = Array.from(arguments);
   } else {
-    this.marks.push(Array.from(arguments));
+    (Array.from(arguments)).forEach(elem => this.marks.push(elem));
   }
 }
 
+Student.prototype.getAverage = function () {
+  this.avg = this.marks.reduce((sum, elem) => sum + elem) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
