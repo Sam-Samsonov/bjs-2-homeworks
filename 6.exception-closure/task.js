4077e9f3e0c3e2e2847ca8b7c5e7a718d8c96620
Sergei Martushev <martuschev.sam@yandex.ru>
2021-11-28 16:30:38 +0300

    hw-6 first commit

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,58 @@
+function parseCount(something) {
+  let num = Number.parseInt(something, 10);
+  if (Number.isNaN(num)) {
+    throw new Error ("Невалидное значение");
+  } else {
+    return num;
+  }
+}
+
+function validateCount(some) {
+  try {
+    return parseCount(some);
+  }
+  catch(error) {
+    return error;
+  }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a >= b +c) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.a = a;
+        }
+        if (b >= a + c) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.b = b;
+        }
+        if (c >= a + b) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.c = c;
+        }
+    }
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        let p = this.getPerimeter() * 0.5;
+        return +((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  }
+  catch(Error) {
+    return {
+      getArea: () => 'Ошибка! Треугольник не существует',
+      getPerimeter: () => 'Ошибка! Треугольник не существует'
+    };
+  }
+}
