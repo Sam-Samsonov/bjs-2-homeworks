40e267bd430f8a9eb1f0c5168e20a8a4832e866e
Sergei Martushev <martuschev.sam@yandex.ru>
2021-12-09 14:22:23 +0300

    hw-8 first commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,56 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash === hash);
+
+    if (idx !== -1 ) {
+        console.log("Из кэша: " + cache[idx].result);
+        return "Из кэша: " + cache[idx].result;
+    }
+
+    let result = func(...args);
+    cache.push({hash, result}) ;
+
+    if (cache.length > 5) {
+      cache.shift();
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timer = null;
+
+  function wrapper(...args) {
+    if (timer === null) {
+      func(...args);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {func(...args)}, ms);
+  }
+
+return wrapper;
+}
+
+function debounceDecorator2(func, ms) {
+  let timer = null;
+  wrapper.count = 0;
+  function wrapper(...args) {
+    if (timer === null) {
+      func(...args);
+    }
+    wrapper.count += 1;
+    clearTimeout(timer);
+    timer = setTimeout(() => {func(...args)}, ms);
+  }
+
+return wrapper;
 }
